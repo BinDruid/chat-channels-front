ab98f946cfe6ded45a9b1b4e86665620659b954e
BinDruid <abharya.dev@gmail.com>
2023-01-24 21:26:36 +0330

    feat: user can delete his own message by cliking on it

--- src/pages/TheChats.vue
+++ src/pages/TheChats.vue
@@ -5,7 +5,7 @@
             <v-card class="overflow-auto mx-auto" fill-height>
                 <v-list>
                     <v-list-item v-for="(chat, index) in chats" :key="index" class="ma-2" :value="chat"
-                        active-color="pink-lighten-2" rounded="xl">
+                        active-color="pink-lighten-2" rounded="xl" @click="deleteChat(chat)">
                         <v-chip class="ma-1" color="pink" size="large" label text-color="white">
                             {{ chat["content"] }}
                         </v-chip>
@@ -42,18 +42,20 @@ const { status, data, send, open, close } = useWebSocket(`${SOCKET_URL}/${props.
 const refineData = (data) => JSON.parse(data.value.replace("\\", ''))
 
 watch((data), () => {
-    const socke_response = refineData(data)
-    if (socke_response.type === "show_chat_message")
-        chats.value.push(socke_response["message"])
-    if (socke_response.type === "recent_messages")
-        for (const chat of socke_response["messages"])
-            chats.value.push(chat)
+    const server_response = refineData(data)
+    if (server_response.type === "new_chat_message")
+        chats.value.push(server_response["message"])
+    if (server_response.type === "server_recent_messages")
+        chats.value = chats.value.concat(server_response["messages"])
+    if (server_response.type === "delete_chat_message") {
+        chats.value = chats.value.filter(chat => chat.id !== server_response["id"])
+    }
 })
 
 
 const sendMessage = () => {
     send(JSON.stringify({
-        type: "save_chat_message",
+        type: "client_new_chat",
         message: message.value
     }))
     clearMessage()
@@ -61,6 +63,16 @@ const sendMessage = () => {
 
 const clearMessage = () => {
     message.value = null
+}
+
+const sentByCurrentUser = (chat) => { return chat.chatter.username === localStorage.getItem("username") }
+
+const deleteChat = (chat) => {
+    if (sentByCurrentUser(chat))
+        send(JSON.stringify({
+            type: "client_delete_chat",
+            id: chat.id
+        }))
 }
 
 </script>
